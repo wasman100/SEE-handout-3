e5d1531944f914c913296f068e08d6ab8ece3baf
wesley sharpe <wesmon50@yahoo.com>
2020-09-18 15:31:45 -0400

    rubber banding is banded

--- src/drawLines.java
+++ src/drawLines.java
@@ -1,8 +1,29 @@
+import acm.graphics.*;
 import acm.program.*;
+import java.awt.event.*; 
 
-public class drawLines extends ConsoleProgram{
+public class drawLines extends GraphicsProgram{
 	 public void run() {
+		addMouseListeners();
+		addMouseMotionListener(this);
+		
 		 
-	 } 
+	 }
 
+	public void mouseClicked(MouseEvent e) {
+		 double x = e.getX();
+		 double y = e.getY();
+		 
+		 line = new GLine(x,y, x,y);
+		 add(line);
+	 }
+
+	 public void mouseMoved(MouseEvent e) {
+		 double x = e.getX();
+		 double y = e.getY();
+		 
+		 line.setEndPoint(x, y);
+		 
+	 }
+	 private GLine line;
 }
