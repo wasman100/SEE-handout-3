6eef1e0d0ab9e5a7742296a31a22b94709daa101
wesley sharpe <sharpe.wesley.ws@gmail.com>
2020-09-27 01:05:20 -0400

    cleaned up

--- src/drawLines.java
+++ src/drawLines.java
@@ -1,29 +1,29 @@
 import acm.graphics.*;
 import acm.program.*;
-import java.awt.event.*; 
+import java.awt.event.*;
 
-public class drawLines extends GraphicsProgram{
-	 public void run() {
+public class drawLines extends GraphicsProgram {
+	public void run() {
 		addMouseListeners();
 		addMouseMotionListener(this);
-		
-		 
-	 }
+
+	}
 
 	public void mouseClicked(MouseEvent e) {
-		 double x = e.getX();
-		 double y = e.getY();
-		 
-		 line = new GLine(x,y, x,y);
-		 add(line);
-	 }
+		double x = e.getX();
+		double y = e.getY();
 
-	 public void mouseMoved(MouseEvent e) {
-		 double x = e.getX();
-		 double y = e.getY();
-		 
-		 line.setEndPoint(x, y);
-		 
-	 }
-	 private GLine line;
+		line = new GLine(x, y, x, y);
+		add(line);
+	}
+
+	public void mouseMoved(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+
+		line.setEndPoint(x, y);
+
+	}
+
+	private GLine line;
 }
--- src/traceing.java
+++ src/traceing.java
@@ -1,5 +1,4 @@
 
-
 /*
  * a) The value of a local variable named i has no direct relationship with that of a
 variable named i in its caller. True
@@ -9,31 +8,33 @@ named x in its caller. True
 
 import acm.program.*;
 
-public class traceing extends ConsoleProgram{
+public class traceing extends ConsoleProgram {
 	public void run() {
 		/* You fill this in */
-		 bludger(2001);
-		 }
-		 private void bludger(int y) // y = 2001
-		 {
-		 int x = y / 1000; //x = 2
-		 int z = (x + y); // z = 2003
-		 x = quaffle(z, y); // x  = 1
-		 println("bludger: x = " + x + ", y = " + y + ", z = " + z); // output is "bludger: x = 1, y = 2001, z = 2003"
-		 }
-		 private int quaffle(int x, int y) // x = 2003 y = 2001
-		 {
-		 int z = snitch(x + y, y); // z = 1001
-		 y /= z; // 1 
-		 println("quaffle: x = " + x + ", y = " + y + ", z = " + z);// output is "quaffle x = 2003, y = 1, z = 1001"
-		 return z; // 1
-		 }
-		 private int snitch(int x, int y) // x = 4004 y = 2001
-		 {
-		 y = x / (x % 10); // 4004/4 y = 1001
-		 println("snitch: x = " + x + ", y = " + y); // print is "snitch: x = 4004, y = 1001
-		 return y; // 1001
-		 }
-		
+		bludger(2001);
+	}
+
+	private void bludger(int y) // y = 2001
+	{
+		int x = y / 1000; // x = 2
+		int z = (x + y); // z = 2003
+		x = quaffle(z, y); // x = 1
+		println("bludger: x = " + x + ", y = " + y + ", z = " + z); // output is "bludger: x = 1, y = 2001, z = 2003"
+	}
+
+	private int quaffle(int x, int y) // x = 2003 y = 2001
+	{
+		int z = snitch(x + y, y); // z = 1001
+		y /= z; // 1
+		println("quaffle: x = " + x + ", y = " + y + ", z = " + z);// output is "quaffle x = 2003, y = 1, z = 1001"
+		return z; // 1
+	}
+
+	private int snitch(int x, int y) // x = 4004 y = 2001
+	{
+		y = x / (x % 10); // 4004/4 y = 1001
+		println("snitch: x = " + x + ", y = " + y); // print is "snitch: x = 4004, y = 1001
+		return y; // 1001
+	}
 
 }
