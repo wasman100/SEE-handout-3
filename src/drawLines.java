9711def103177db97ea2b792b0e68296e410ff82
wesley sharpe <sharpe.wesley.ws@gmail.com>
2020-09-29 19:26:38 -0400

    made it to 20 lines

--- src/drawLines.java
+++ src/drawLines.java
@@ -3,9 +3,7 @@ import acm.program.*;
 import java.awt.event.*;
 
 public class drawLines extends GraphicsProgram {
-	public void run() {
-		addMouseListeners();
-	}
+	public void run() {addMouseListeners();}
 	public void mousePressed(MouseEvent e) {
 		double x = e.getX();
 		double y = e.getY();
@@ -18,6 +16,5 @@ public class drawLines extends GraphicsProgram {
 		line.setEndPoint(x, y);
 	}
     public void mouseReleased(MouseEvent e) {}
-
 	private GLine line;
 }
