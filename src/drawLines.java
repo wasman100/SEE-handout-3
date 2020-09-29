ccd68eed6e802bacf5257c1429fb8fd12416d097
wesley sharpe <sharpe.wesley.ws@gmail.com>
2020-09-29 19:21:37 -0400

    update

--- src/drawLines.java
+++ src/drawLines.java
@@ -7,23 +7,18 @@ public class drawLines extends GraphicsProgram {
 		addMouseListeners();
 		addMouseMotionListener(this);
 	}
-
-	public void mouseClicked(MouseEvent e) {
+	public void mousePressed(MouseEvent e) {
 		double x = e.getX();
 		double y = e.getY();
-		
-
 		line = new GLine(x, y, x, y);
-		line.setStartPoint(x, y);
 		add(line);
-	}
-
+	    }
 	public void mouseDragged(MouseEvent e){
 		double x = e.getX();
 		double y = e.getY();
-
 		line.setEndPoint(x, y);
 	}
+    public void mouseReleased(MouseEvent e) {}
 
 	private GLine line;
 }
