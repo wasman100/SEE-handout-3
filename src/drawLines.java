16eed669917f6ef0e973d30acf3f6d30930dddbc
wesley sharpe <sharpe.wesley.ws@gmail.com>
2020-10-01 09:25:33 -0400

    less than 20 yes

--- src/drawLines.java
+++ src/drawLines.java
@@ -3,17 +3,15 @@ import acm.program.*;
 import java.awt.event.*;
 
 public class drawLines extends GraphicsProgram {
-	public void run() {addMouseListeners();}
+	public void run() {
+		addMouseListeners();
+		}
 	public void mousePressed(MouseEvent e) {
-		double x = e.getX();
-		double y = e.getY();
-		line = new GLine(x, y, x, y);
+		line = new GLine(e.getX(), e.getY(), e.getX(), e.getY());
 		add(line);
 	    }
 	public void mouseDragged(MouseEvent e){
-		double x = e.getX();
-		double y = e.getY();
-		line.setEndPoint(x, y);
+		line.setEndPoint(e.getX(), e.getY());
 	}
     public void mouseReleased(MouseEvent e) {}
 	private GLine line;
